ddc14196b4ac37d80f348776b33a2e3895813ab0
xuyishen <xzj2495462405@outlook.com>
2020-03-27 15:09:26 +0800

    GAUSSIANBLUR

---
+++ test/test/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 徐亦燊 on 2020/2/28.
+//  Copyright © 2020 徐亦燊. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    
+    VideoCapture cap(0);
+    
+    
+    while(1)
+    {
+        Mat InMat;
+        Mat OutMat;
+        cap>>InMat;
+        Sobel(InMat,OutMat,CV_1);
+        imshow("InMat",InMat);
+        imshow("OutMat",OutMat);
+        
+        waitKey(30);
+        
+    }
+    }
